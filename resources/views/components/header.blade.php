71c10577953e68a69b809e1b708f4c2bfa28e669
enju <enjudev@outlook.com>
2024-01-14 18:31:50 +0700

    .

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,10 +1,48 @@
 <section id="headerMobile" class="tw-block lg:tw-hidden">
-    <div class="tw-fixed tw-left-0 tw-top-0 tw-z-[9999] tw-flex tw-w-full tw-bg-white tw-px-15px tw-py-15px">
+    <div class="tw-fixed tw-left-0 tw-top-0 tw-z-[200] tw-flex tw-w-full tw-bg-white tw-px-15px tw-py-15px">
         <a href="" class="tw-h-auto tw-w-150px">
             <img src="{{ asset('assets/Logo.svg') }}" class="object-contain" alt="" />
         </a>
+        <div class="tw-ml-auto">
+            <button class="" type="button" data-drawer-target="mobileDrawer" data-drawer-show="mobileDrawer"
+                aria-controls="mobileDrawer">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                    fill="none">
+                    <path d="M3 4.5H21" stroke="#292D32" stroke-width="1.5" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                    <path d="M3 9.5H21" stroke="#292D32" stroke-width="1.5" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                    <path d="M3 14.5H21" stroke="#292D32" stroke-width="1.5" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                    <path d="M3 19.5H21" stroke="#292D32" stroke-width="1.5" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                </svg>
+            </button>
+        </div>
     </div>
 </section>
+<div id="mobileDrawer"
+    class="tw-fixed tw-left-0 tw-top-0 tw-z-[300] tw-h-screen tw-w-[320px] -tw-translate-x-full tw-bg-white tw-p-16px tw-transition-transform dark:tw-bg-gray-800"
+    tabindex="-1" aria-labelledby="drawer-label">
+    <h5 id="drawer-label"
+        class="inline-flex items-center mb-4 text-base font-semibold text-gray-500 dark:text-gray-400">Medx</h5>
+    <button type="button" data-drawer-hide="mobileDrawer" aria-controls="mobileDrawer"
+        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white">
+        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+        </svg>
+        <span class="sr-only">Close menu</span>
+    </button>
+    <div class="tw-flex tw-flex-col tw-gap-15px">
+        <a href="" class="">HOME</a>
+        <a href="">PERSONAL CARE</a>
+        <a href="">FAMILY CARE</a>
+        <a href="">BLOG</a>
+        <a href="">BRAND</a>
+        <a href="">PAGES</a>
+    </div>
+</div>
 <section id="headerPc" class="hidden lg:block tw-relative">
     <div
         class="tw-fixed tw-left-30px tw-top-30px tw-z-50 tw-h-110px tw-w-[calc(100%-60px)] tw-rounded-15px tw-bg-white tw-shadow-md">
